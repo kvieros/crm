987b3eff59d9ce36cbf0e93be7706062045628a2
kyr <kvieros@hotmail.com>
2017-10-09 14:19:05 +0300

    commit from new branch workbranch

---
+++ app/Http/Controllers/email/SendEmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\email;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class SendEmail extends Controller
+{
+    //
+}
